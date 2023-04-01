6d368ca4ce5ba74733b77ca72c1a2ea1c8fafa3c
lamsyA <olamide.adetula.1744@gmail.com>
2023-04-02 00:06:39 +0100

    Minting Component

--- src/views/Asset.jsx
+++ src/views/Asset.jsx
@@ -1,10 +1,12 @@
 import AssetDetails from "../component/AssetDetails"
+import Buyer from "../component/Buyer"
 
 
 const Asset = () => {
     return (
         <>
             <AssetDetails />
+            <Buyer/>
         </>
     )
 }
