a1f12c37712c54ce1a24cfa1317a813ec96b7463
LamsyA <olamide.adetula.1744@gmail.com>
2023-04-15 22:02:18 +0100

    status

--- src/component/Assets.jsx
+++ src/component/Assets.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {MdReport, MdVerified} from 'react-icons/md'
 
 
 const Assets = ({assets}) => {
@@ -15,7 +16,7 @@ const Assets = ({assets}) => {
 }
 
 const AssetCard = ({ asset }) => (
-    <div id='assets' className='rounded-lg shadow-lg bg-white w-64 m-4'>
+    <div id='assets' className='rounded-lg shadow-lg bg-white w-64 m-4 hover:shadow-red-300'>
         <Link to={'/assets/' + asset.id}>
             <img
                 src={ asset.credential||'https://media.wired.com/photos/5926e641f3e2356fd800ad1d/master/w_2560%2Cc_limit/AnkiTA.jpg'}
@@ -23,7 +24,7 @@ const AssetCard = ({ asset }) => (
                 className='rounded-xl h-64 w-full object-cover'
             />
             <div className='p-4 '>
-                <h4 className='text-sm mb-4'>Asset Title: {asset.title}</h4>
+                <h4 className='text-sm mb-4 text-yellow-600'>Asset Title: {asset.title}</h4>
                 <div className='flex flex-row mb-2 justify-between items-center space-x-2'>
                     <div className='flex '>
                         <small className='text-gray-700'>{asset.holder.slice(0,5) +"..."+ asset.holder.slice(-5) }</small>
@@ -35,13 +36,27 @@ const AssetCard = ({ asset }) => (
                 leading-none rounded-l-full h-1 text-teal-100 text-center'
                         style={{ width: `${asset.status * 25}%` }}> </div>
                 </div>
+                <div className='flex justify-between items-center mt-2 mb-2'>
+                    <small className='text-blue-600'>Created: {asset.timestamp}</small> 
+                    {asset.probe ? ( 
+                        <small className='flex justify-start items-center'><span>Held </span>  
+                    <MdReport className='text-red-500' size={20}/>
+                    </small>
+                    ): (
+                        <small className='flex justify-start items-center'><span>Verified </span>  
+                    <MdVerified className='text-green-500' size={20}/>
+                    </small>
+                    )}
+                    
+                </div>
                 <div className='flex justify-between items-center flex-wrap mt-4 
                 mb-2 text-gray-500 font-bold'>
                     
-                    <small className='text-gray-500 text-sm'> Created: {asset.timestamp}</small>
+                    <small className='text-gray-500 text-sm'> Price: {asset.price} ETH</small>
 
                     <div>
                         <small > Open</small>
+                        
                     </div>
                 </div>
             </div>
