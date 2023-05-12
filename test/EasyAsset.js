7b6a8805655fbf512d28429a018eebe488221999
LamsyA <olamide.adetula.1744@gmail.com>
2023-05-12 13:10:36 +0100

    test driven development

--- test/EasyAsset.js
+++ test/EasyAsset.js
@@ -2,7 +2,7 @@ const {expect} = require('chai');
 const {ethers} = require('hardhat')
 
 describe('EasyAsset', () => {
-    let EasyAsset, contract,owner, addr2, addr1
+    let EasyAsset, contract,owner, buyerAddress, addr1
 
     // parameters for creating an Asset
     const id = 0;
@@ -17,7 +17,8 @@ describe('EasyAsset', () => {
 
 beforeEach( async () => {
         EasyAsset = await ethers.getContractFactory("EasyAsset");
-        [owner, addr2, addr1] = await ethers.getSigners();
+        [owner, buyerAddress, addr1] = await ethers.getSigners();
+        // console.log("owner: " + owner.address);
         contract = await EasyAsset.deploy( "EasyAsset", "EAS")
         await contract.deployed();
     })
@@ -25,56 +26,66 @@ beforeEach( async () => {
 describe(" Asset Creation ", () => {
        beforeEach( async() => {
           const b = await contract.createAsset(title,  description, credential, price)
-        //   console.log("bb ", b)
+        //   console.log("bb ", b.address)
 
         })
         it('should contain  list of  created Asset', async() => {
             result = await contract.getAssets()
+            // console.log("bb->>>>>>>>>>>>>> ", result)
             expect(result).to.have.length(1);
             
         })
-        it('should be able to buy Asset and revert with buyer address as the owner', async() => {
+        beforeEach( async() => {
             result = await contract.getAsset(id)
-            await contract.connect(addr2).buyAsset(0, {
+            await contract.connect(buyerAddress).buyAsset(0, {
                 value: price
               })
-            result = await contract.getAsset(id) 
-            expect(await contract.ownerOf(id)).to.revertedWith(addr2.address)
+            result = await contract.getBuyer(id) 
+            // console.log("get buyer", result)
+            expect(result.owner).to.be.eq(buyerAddress.address)
             // expect(result.status).to.equal(1)
         })
+        it('should  refund buyer', async() => {
+            result = await contract.getBuyer(id);
+            buyer =  await contract.connect(buyerAddress).refund(id);
+ 
+             result = await contract.getAsset(id);
+            //  console.log("get buyer", buyer)
+ 
+ 
+            expect(buyer).to.emit(result.staus, buyer.status)
+ 
+         })
         it('should check the status of the Asset', async() => {
-            result = await contract.getAsset(0)
-         const r =   await contract.connect(addr1).buyAsset(id, { 
-                value: price
-              })
-            //   console.log("rrrr ", r)
+           
             result = await contract.getAsset(0) 
             
             expect(result.status).to.equal(1)
         })
         it('should check for the confirmation of the Asset', async() => {
-            result = await contract.getBuyer(0)
-            // console.log("addr2 ",result)
-            await contract.connect(addr1).confirm(0)
+            // result = await contract.getBuyer(0)
+            // console.log("buyerAddress ",result)
+           result = await contract.connect(buyerAddress).confirm(0)
             result = await contract.getAsset(0) 
             // console.log("result", result)
             expect(result.status).to.equal(4)
         })
-        it('should  refund buyer', async() => {
-        //    result = await contract.getBuyer(0);
-            await contract.connect(addr2).refund(0);
-           buyer = await contract.getBuyer(0);
-
-           expect(await contract.ownerOf(id)).to.be.revertedWith(addr2.address)
-
-        })
+       
         it('should  probe asset', async() => {
             // result = await contract.getBuyer(0);
              const buyer = await contract.Probe(0, {from: owner.address});
             result = await contract.getAsset(0);
 
-             expect(result.status).to.equal(5);
+             expect(result.status).to.equal(2);
             //  expect(result.refunded).to.equal(true);
+         })
+         it('should get the buyer of the asset', async() => {
+
+            result = await contract.getBuyer(id);
+            // console.log('list of buyer', result);
+
+             expect(result.owner).to.be.eq(buyerAddress.address);
+            //  expect(result.owner).to.equal(true);
          })
         
      })
